c7e9b20d42289e92c062131982868b816d5979ee
HinsLeung <2597639004@qq.com>
2024-08-02 22:06:07 +0800

    8.2.2

--- src/main/java/com/hins/hinsoj/judge/codesandbox/impl/ExampleCodeSandbox.java
+++ src/main/java/com/hins/hinsoj/judge/codesandbox/impl/ExampleCodeSandbox.java
@@ -3,14 +3,33 @@ package com.hins.hinsoj.judge.codesandbox.impl;
 import com.hins.hinsoj.judge.codesandbox.CodeSandbox;
 import com.hins.hinsoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.hins.hinsoj.judge.codesandbox.model.ExecuteCodeResponse;
+import com.hins.hinsoj.model.dto.questionsubmit.JudgeInfo;
+import com.hins.hinsoj.model.enums.JudgeInfoMessageEnum;
+import com.hins.hinsoj.model.enums.QuestionSubmitStatusEnum;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 /**
  * 示例代码沙箱
  */
+@Slf4j
 public class ExampleCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        List<String> inputList = executeCodeRequest.getInputList();
+
+        ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+        executeCodeResponse.setOutputList(inputList);
+        executeCodeResponse.setMessage("测试执行成功");
+        executeCodeResponse.setStatus(QuestionSubmitStatusEnum.SUCCESS.getValue());
+        JudgeInfo judgeInfo = new JudgeInfo();
+        judgeInfo.setMessage(JudgeInfoMessageEnum.ACCEPTED.getText());
+        judgeInfo.setMemory(100L);
+        judgeInfo.setTime(100L);
+
+        executeCodeResponse.setJudgeInfo(judgeInfo);
         System.out.println("示例代码沙箱");
-        return null;
+        return executeCodeResponse;
     }
 }

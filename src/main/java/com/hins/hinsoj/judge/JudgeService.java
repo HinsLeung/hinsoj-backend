463b2b8ee84914bef6bc0981af0f09c1dcb74a59
HinsLeung <2597639004@qq.com>
2024-08-04 01:19:42 +0800

    8.3.1

--- src/main/java/com/hins/hinsoj/judge/JudgeService.java
+++ src/main/java/com/hins/hinsoj/judge/JudgeService.java
@@ -1,4 +1,16 @@
 package com.hins.hinsoj.judge;
 
+import com.hins.hinsoj.model.vo.QuestionSubmitVO;
+
+/**
+ * 判题服务
+ */
 public interface JudgeService {
+
+    /**
+     * 判题
+     * @param questionSubmitId
+     * @return
+     */
+    QuestionSubmitVO doJudge(long questionSubmitId);
 }
